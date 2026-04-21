1eb5d5c4308913f700d40a81f26c93b0d0222a3e
barq e insaf <rezan.sohail01@gmail.com>
2026-04-21 10:54:27 +0500

    Added new screens

--- BarqEInsafApp/app/(tabs)/index.tsx
+++ BarqEInsafApp/app/(tabs)/index.tsx
@@ -1,23 +1,5 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { Redirect } from 'expo-router';
 
-export default function HomeScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello World</Text>
-    </View>
-  );
+export default function Index() {
+  return <Redirect href={'../SplashScreen' as any} />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-});
